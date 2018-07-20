61a270ee94910d76a7244811a873b5fbd25cf3e6
David Hugh-Jones <davidhughjones@gmail.com>
2018-07-20 08:50:03 +0000

    version 3.5.1.0

---
+++ R/rcheology.R
@@ -0,0 +1,108 @@
+
+#' Data on base packages from current and previous versions of R
+#' 
+#' rcheology is a data package providing two data frames: 
+#' 
+#' * [rcheology] lists objects in all versions of R from 2.0.0 onwards.
+#' * [Rversions] lists R versions and their release dates.
+#' 
+#' The version of the rcheology package reflects the latest R version to be included in the data, 
+#' e.g. 3.5.1.x contains data up to and including R 3.5.1.
+#' 
+#' An online app for data exploration is available at <https://hughjonesd.shinyapps.io/rcheology/>.
+#' 
+#' @section How the data is created:
+#' 
+#' Previous R versions are downloaded and installed on a Docker image. Objects in 
+#' all base packages are then listed. (Note that recommended packages, like `nnet` or `MASS`,
+#' are not installed.) For functions, the [formals()] of the function are recorded.
+#' 
+#' Versions 3.0.1 and up are installed from the 
+#' [CRAN apt repositories for Ubuntu Trusty Tahr](https://cran.r-project.org/bin/linux/ubuntu/trusty/).
+#'  Version 3.5.0 and up use a 
+#' [special repository](https://cran.r-project.org/bin/linux/ubuntu/trusty-cran35/).
+#' 
+#' Versions 2.5.1 to 3.0.0 are built from source on [Ubuntu Lucid Lynx](https://hub.docker.com/r/yamamuteki/ubuntu-lucid-i386/).
+#' 
+#' Versions 2.0.0 to 2.4.1 are built from source on [Debian Sarge](https://hub.docker.com/r/debian/eol/).
+#' 
+#' @docType package
+#' @name rcheology-package
+NULL
+
+#' Data on objects from current and previous versions of R
+#' 
+#' A data frame with every function (and other object) in versions
+#' of R from 2.0.0 onwards. Variables are:
+#' 
+#' * `name`: name of the object
+#' * `type`: Result of calling [typeof()] on the object
+#' * `class`: [class()] of the object, separated by slashes if there are multiple classes.
+#' * `generic`: `TRUE` if the object is an (S4) generic according to [methods::isGeneric()]
+#' * `args`: the arguments of the function, or NA for non-functions
+#' * `package`: package the object comes from
+#' * `Rversion`: version of R as major.minor.patch
+#' 
+#' @name rcheology 
+NULL
+
+
+
+#' Previous R versions with dates
+#' 
+#' A data frame with 2 variables:
+#' * `Rversion`: version of R as major.minor.patch
+#' * `date`: date of release
+#' 
+#' This goes back to 0.x releases. Some version 1's are "recommended";
+#' I don't know what these are. For 2.15.1-w, see 
+#' [here](https://cran.r-project.org/src/base/R-2/README-2.15.1-w).
+#' 
+#' @name Rversions 
+NULL
+
+
+
+#' Check if a core R function changed between R versions
+#'
+#' @param fn Character name of a function in a core R package.
+#' @param from Minimum R version (optional).
+#' @param to Maximum R version (optional).
+#' @param package Name of the package (optional).
+#' 
+#' @return 0 if there was no change. 1 if the function's arguments changed.
+#'   2 if the function was not present in all versions. If the function can't
+#'   be found or exists in multiple packages, throws an error.
+#' @export
+#'
+#' @examples
+#' fun_changed("debugonce")
+#' \donttest{
+#' fun_changed("debugonce", "3.4.0", "3.4.3")
+#' fun_changed("debugonce", "3.3.0", "3.4.3")
+#' }
+fun_changed <- function (fn, from = NULL, to = NULL, package = NULL) {
+  rch <- rcheology::rcheology
+  vns <- as.package_version(rch$Rversion) 
+  # unique.character MUCH faster than unique.package_version:
+  relevant_vns <- as.package_version(unique(rch$Rversion))
+  range <- rch$name == fn
+  if (! is.null(package)) range <- range & rch$package == package
+  if (! is.null(from))    {
+    from <- as.package_version(from)
+    range <- range & vns >= from
+    relevant_vns <- relevant_vns[relevant_vns >= from]
+  }
+  if (! is.null(to)) {
+    to   <- as.package_version(to)
+    range <- range & vns <= to
+    relevant_vns <- relevant_vns[relevant_vns <= to]
+  }
+  fns <- rch[range, , drop = FALSE]
+  args <- fns$args
+  if (length(unique(fns$package)) > 1) stop("Multiple functions found with that name")
+  if (nrow(fns) == 0) stop("Couldn't find function of those versions")
+  if (nrow(fns) < length(relevant_vns)) return(2)
+  if (length(unique(args)) > 1) return(1)
+  return(0)
+}
---
+++ README.md
@@ -0,0 +1,93 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+rcheology
+=========
+
+[![Travis build status](https://travis-ci.org/hughjonesd/rcheology.svg?branch=master)](https://travis-ci.org/hughjonesd/rcheology) [![AppVeyor build status](https://ci.appveyor.com/api/projects/status/github/hughjonesd/rcheology?branch=master&svg=true)](https://ci.appveyor.com/project/hughjonesd/rcheology)
+
+A data package which lists every command in base R packages since R version 2.0.0.
+
+The latest R version covered is 3.5.1.
+
+Installing
+----------
+
+``` r
+install.packages("remotes") # if you need to
+remotes::install_github("hughjonesd/rcheology")
+```
+
+Where the data comes from
+-------------------------
+
+Versions 3.0.1 and up are installed from the [CRAN apt repositories for Ubuntu Trusty Tahr](https://cran.r-project.org/bin/linux/ubuntu/trusty/). Version 3.5.0 and up use a [special repository](https://cran.r-project.org/bin/linux/ubuntu/trusty-cran35/).
+
+Versions 2.5.1 to 3.0.0 are built from source on [Ubuntu Lucid Lynx](https://hub.docker.com/r/yamamuteki/ubuntu-lucid-i386/).
+
+Versions 2.0.0 to 2.4.1 are built from source on [Debian Sarge](https://hub.docker.com/r/debian/eol/).
+
+Results are found from running `ls` on all installed packages from a minimal installation. Recommended packages are not included.
+
+The `Rversions` data frame lists versions of R and release dates.
+
+Do it yourself
+--------------
+
+-   Install docker
+-   `./control build` builds the images. Or get them from <https://hub.docker.com/r/dash2/rcheology/>.
+-   `./control run` runs the images to build/install R and extract data
+-   `./control gather` gets CSV files from the containers
+-   `./control write` puts CSV files into a data frame and stores it in the package
+
+The data
+--------
+
+You can view the data online in a [Shiny app](https://hughjonesd.shinyapps.io/rcheology/).
+
+``` r
+library(rcheology)
+data("rcheology")
+
+head(rcheology)
+#>                name    type    class generic          args package
+#> 1                 ! builtin function   FALSE          <NA>    base
+#> 2                != builtin function   FALSE          <NA>    base
+#> 3                 $ special function   FALSE          <NA>    base
+#> 4 $.package_version closure function   FALSE     (x, name)    base
+#> 5               $<- special function   FALSE          <NA>    base
+#> 6    $<-.data.frame closure function   FALSE (x, i, value)    base
+#>   Rversion
+#> 1    2.0.0
+#> 2    2.0.0
+#> 3    2.0.0
+#> 4    2.0.0
+#> 5    2.0.0
+#> 6    2.0.0
+```
+
+Base functions over time:
+
+``` r
+library(ggplot2)
+suppressPackageStartupMessages(library(dplyr))
+#> Warning: package 'dplyr' was built under R version 3.5.1
+
+rvs <- rcheology$Rversion     %>% 
+      unique()                %>% 
+      as.package_version()    %>% 
+      sort() %>% 
+      as.character()
+
+major_rvs <- grep(".0$", rvs, value = TRUE)
+major_rv_dates <- Rversions$date[Rversions$Rversion %in% major_rvs]
+
+rch_dates <- rcheology %>% left_join(Rversions, by = "Rversion")
+ggplot(rch_dates, aes(date, group = package, fill= package), colour = NA) + 
+      stat_count(geom = "area") + 
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) + 
+      ggthemes::scale_fill_gdocs() +
+      scale_x_date(breaks  = major_rv_dates, labels = major_rvs) + 
+      xlab("Version") + ylab("Function count")
+```
+
+<img src="man/figures/README-unnamed-chunk-3-1.png" width="100%" />
